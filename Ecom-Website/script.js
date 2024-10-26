0dacfa961f336fa4a2802df50eaf8d3578d5add6
Amit Naik <154658171+Amitnaik003@users.noreply.github.com>
2024-10-26 08:59:30 +0530

    Update script.js

--- Ecom-Website/script.js
+++ Ecom-Website/script.js
@@ -6,7 +6,7 @@ const signUpForm=document.getElementById('signup');
 signUpButton.addEventListener('click',function(){
     signInForm.style.display="none";
     signUpForm.style.display="block";
-})
+}}
 signInButton.addEventListener('click', function(){
     signInForm.style.display="block";
     signUpForm.style.display="none";
